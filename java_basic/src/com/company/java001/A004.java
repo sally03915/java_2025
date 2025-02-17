d08900e12de5d9eb6cffcc9e8446da9f725301f8
sally An <sally03915@gmail.com>
2025-02-17 18:03:32 +0900

    2025-02-17     java001_setting / class /  처음으로 작성해본 자바

---
+++ java_basic/src/com/company/java001/A004.java
@@ -0,0 +1,15 @@
+package com.company.java001;
+
+public class A004 {
+   public static void main(String[] args) {
+	   System.out.println("1. 파일명저장  : A004.java ");
+	   System.out.println("2. 클래스 몇개 : 5개");
+	   System.out.println("3. 클래스이름  : A004.class / A41.class / A42.class/ A43.class/ A43$A44.class  ");
+   }
+}
+
+class A41{}
+class A42{}
+class A43{
+	class A44{}
+}
